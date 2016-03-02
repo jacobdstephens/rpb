86da489e34a13f52005a69045bff63e1518dd419
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-02 16:08:42 -0500

    comented out socket io section from app.js

--- app.js
+++ app.js
@@ -3,29 +3,27 @@ var app = express();
 app.use(express.static('public'));
 var http = require('http').Server(app);
 var port = 5000;
- 
-http.listen(port, function() {
-    console.log('listening on *: ' + port);
-});
 
 app.get('/:file', function(req, res){
   res.sendFile(__dirname + '/' + req.params.file);
 });
 
 // setup my socket server
-var io = require('socket.io')(http);
+//var io = require('socket.io')(http);
  
-io.on('connection', function(socket) {
-    console.log('New connection');
+//io.on('connection', function(socket) {
+//    console.log('New connection');
  
-    socket.on('message', function(msg) {
-        console.log('Got message from client: ' + msg);
-    }
-}
+//    socket.on('message', function(msg) {
+//        console.log('Got message from client: ' + msg);
+//    }
+//}
 
 // Called when the client calls socket.emit('move')
-socket.on('move', function(msg) {
-    socket.broadcast.emit('move', msg);
+//socket.on('move', function(msg) {
+//    socket.broadcast.emit('move', msg);
+//});
+
+http.listen(port, function() {
+    console.log('listening on *: ' + port);
 });
-
-
