25792ad394e99e591ee4ee73c3b2d1273b6d66df
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-03 03:29:03 -0500

    fixing more syntax

--- app.js
+++ app.js
@@ -17,11 +17,10 @@ io.on('connection', function(socket) {
     socket.on('message', function(msg) {
         console.log('Got message from client: ' + msg);
     });
-});
-
-// Called when the client calls socket.emit('move')
-socket.on('move', function(msg) {
-    socket.broadcast.emit('move', msg);
+	// Called when the client calls socket.emit('move')
+	socket.on('move', function(msg) {
+		socket.broadcast.emit('move', msg);
+	});
 });
 
 http.listen(port, function() {
