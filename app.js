bba599115e9103d67ecd5a0271dc10e1f26a9a05
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-02 19:07:59 -0500

    Fixing syntax on line endings

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ io.on('connection', function(socket) {
  
     socket.on('message', function(msg) {
         console.log('Got message from client: ' + msg);
-    }
+    });
 });
 
 // Called when the client calls socket.emit('move')
