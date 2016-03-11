f5363600358224e0682f6ba56ef0f0c747ba3a2c
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-10 22:29:07 -0500

    commented out socket calls in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 $("#play").click(function() { 
-	var socket = io();
+	/*var socket = io();*/
 	var val = $('input[type=radio]:checked').val();
-	socket.emit('message', 'hello world!');
+	/*socket.emit('message', 'hello world!');*/
 	$('p').text("You played " + val );
 });
 
