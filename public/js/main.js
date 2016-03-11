0d9242eecefe37084a774d6790da535dd2cb73b5
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-10 23:25:04 -0500

    add socket call back to main

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,8 @@
 $("#play").click(function() { 
-	/*var socket = io();*/
+	var socket = io();
 	var val = $('input[type=radio]:checked').val();
 	/*socket.emit('message', 'hello world!');*/
 	$('p').text("You played " + val );
-	console.log('a user connected');
 });
 
 
