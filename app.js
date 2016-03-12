0ee8316d67ee11794814167c2d6c3a8af9fc0424
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-12 17:14:47 -0500

    trying to figure out why console log is not working

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var http = require('http').Server(app);
 var port = 5000;
 
 app.get('/:file', function(req, res){
-  res.sendFile(__dirname + '/' + req.params.file);
+	res.sendFile(__dirname + '/' + req.params.file);
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+	console.log('a user connected');
 });
 
 http.listen(port, function() {
-    console.log('listening on *: ' + port);
+	console.log('listening on *: ' + port);
 });
