0b01f147d02eaaad3c58a07f6d7069f46d7204c1
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-12 19:22:14 -0500

    trying to fire alert from socket

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,12 @@
 /*var socket = io();*/
 
 $("#play").click(function() { 
+	var socket = io();
 	var val = $('input[type=radio]:checked').val();
 	/*socket.emit('message', 'hello world!');*/
 	$('p').text("You played " + val );
 	console.log("You played ")
-	alert("You played ");
+	alert("You played "+ val );
 });
 
 
