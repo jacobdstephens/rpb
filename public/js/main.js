c62d6ed3746bb1552c4c113419db48577d7fec7c
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-13 19:44:03 -0400

    better closer warmer

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,16 @@
-
+var socket = io.connect();
+  socket.on('news', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+  });
 
 $("#play").click(function() { 
 	var val = $('input[type=radio]:checked').val();
 	/*socket.emit('message', 'hello world!');*/
 	$('p').text("You played " + val );
 	/*var socket = io.connect();*/
-	console.log("You played ")
-	alert("You played "+ val );
+	console.log("You played ");
+	/*alert("You played "+ val );*/
 });
 
 
