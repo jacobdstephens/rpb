739b55cdb5b51785c44778e867815ebb2f3afa70
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-13 16:29:05 -0400

    please please please

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ $("#play").click(function() {
 	var val = $('input[type=radio]:checked').val();
 	/*socket.emit('message', 'hello world!');*/
 	$('p').text("You played " + val );
-	var io = require('socket.io-client');
+	var socket = io();
 	console.log("You played ")
 	alert("You played "+ val );
 });
