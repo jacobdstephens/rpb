97a19d8acf53d29ab861e04b0b554ad46e225652
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-14 19:02:09 -0400

    Running everything in main.js as a socket call

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,17 @@
-var socket = io.connect();  
+var socket = io.connect();
+  
 socket.on('this', function(msg){
-    $('p').text(msg));
-  });
-
-$("#play").click(function() { 
-	var val = $('input[type=radio]:checked').val();
-	/*socket.emit('message', 'hello world!');*/
-	$('p').text("You played " + val );
-	socket.emit('my other event', { my: val });
-	/*var socket = io.connect();*/
-	console.log("You played "+ val);
-	/*alert("You played "+ val );*/
+	$('p').text(msg));
+	$("#play").click(function() { 
+		var val = $('input[type=radio]:checked').val();
+		$('p').text("You played " + val );
+		socket.emit('my other event', { my: val });
+		console.log("You played "+ val);
+	
+	});
 });
+
+
 
 
 
