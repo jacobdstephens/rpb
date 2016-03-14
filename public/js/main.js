0a99832b0b64a8b0066cdc90a7d938e55e5bd3f4
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-14 19:14:53 -0400

    fixing socket

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,18 @@
-var socket = io.connect();
+/*var socket = io.connect();
   
 socket.on('this', function(msg){
 	$('p').text(msg));
-	$("#play").click(function() { 
-		var val = $('input[type=radio]:checked').val();
-		$('p').text("You played " + val );
-		socket.emit('my other event', { my: val });
-		console.log("You played "+ val);
+});*/
+
+$("#play").click(function() {
+	var socket = io.connect(); 
+	var val = $('input[type=radio]:checked').val();
+	$('p').text("You played " + val );
+	socket.emit('my other event', { my: val });
+	console.log("You played "+ val);
 	
-	});
 });
+
 
 
 
