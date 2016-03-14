4e7fba79f6c5e3caa99da245f62023344b88a51c
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-13 21:33:29 -0400

    $(

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,16 @@
 var socket = io.connect();
-  socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'farta' });
-  });
+  
 
 $("#play").click(function() { 
 	var val = $('input[type=radio]:checked').val();
 	/*socket.emit('message', 'hello world!');*/
 	$('p').text("You played " + val );
+	socket.on('news', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: val });
+  });
 	/*var socket = io.connect();*/
-	console.log("You played ");
+	console.log("You played "+ val);
 	/*alert("You played "+ val );*/
 });
 
