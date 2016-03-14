26c8327e87885fe9c6f5666bff017ea82bdd70c2
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-13 20:36:48 -0400

    ?

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
-	 alert(data);
+	 alert('fart');
   });
 });
 
