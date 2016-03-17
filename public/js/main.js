4fc91923287721cfae1448cbc8da1c75c93d42b6
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-17 10:35:48 -0400

    unifying ids

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ var _myid = 0;
 var _playerCount = 0;
 
 socket().on('playerID', function(id){
-	$("#myPlayerID").text( "myPlayerID " + id);
+	$("#playerID").text( "my playerID " + id);
 });
 
 socket().on('playerCount', function(count){
@@ -15,7 +15,7 @@ $("#play").click(function() {
 	var val = $('input[type=radio]:checked').val();
 	socket.emit('play', val );
 	console.log("You played "+ val);
-	$("#play").text( "You played " + val);
+	$("#my_play").text( "You played " + val);
 });
 
 socket.on('win', function(win){
