a07bf32dd6ab21b5d53732a1815a566c559218f6
Jacob Stephens <jacobdstephens@gmail.com>
2016-03-21 16:10:47 -0400

    why am I so dumb?

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var io = require('socket.io')(server);
 
 server.listen(5000);
 
-app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + "/public"));
 
 app.get('/', function (req, res) {
 	  res.sendFile('index',
